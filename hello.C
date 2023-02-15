b45da38ef09b603edf988ccd29c619c7ddacad94
Sahib Preet Singh <99086310+Sahib61003@users.noreply.github.com>
2023-02-15 20:35:08 +0530

    Created file 1

---
+++ hello.C
@@ -0,0 +1,17 @@
+/* Basic Program
+	 How to print hello world in C.
+Code:
+*/
+
+#include <stdio.h>
+//basic directory to include to perform code in C.
+int main(){
+	  // Making a int main function which returns 0.
+    printf("Hello World \n");
+	  // printf is used to print anything in C. \n is used to give a line break.
+    return 0;
+}
+
+
+
+
