2ddb86f9d0626536e15c4ec5a15619bab0f947a0
Sahib Preet Singh <99086310+Sahib61003@users.noreply.github.com>
2023-02-23 19:41:44 +0530

    Created file 6

---
+++ conditional_statements.C
@@ -0,0 +1,14 @@
+//Conditional Statements: if, if-else, if-else-if ladder, nested if-else, switch-case
+
+/* BASIC:
+Usage of if, else and else if
+if (condition){
+    //code to be executed if condition is true
+}
+else if (condition){
+    //code to be executed if condition is true
+}
+else{
+    //code to be executed if condition is false
+}
+*/
